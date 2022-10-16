68841358a46a38afa82e9769b6c21ef3f114f33a
truly-sparkle <ycc4477@gmail.com>
2022-10-16 20:24:28 +0900

    feat: production server 설정

--- utils/api/index.ts
+++ utils/api/index.ts
@@ -1,7 +1,11 @@
 import Axios from 'axios'
 
+const HOST = process.env.NEXT_PUBLIC_ENV_HOST
+const PORT = process.env.NEXT_PUBLIC_ENV_PORT
+const BASE_URL = `${HOST}:${PORT}`
+
 const axios = Axios.create({
-  baseURL: 'http://localhost:3000',
+  baseURL: BASE_URL,
 })
 
 export default axios
