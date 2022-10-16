fbc8d28faf7d7ffc6bdc4e979ea7b77913f4c740
Yongju Choi <dydwn0928@gmail.com>
2022-10-16 00:27:57 +0000

    상단요약컴포넌트 layout 작업 수행
    Co-authored-by: 용주 최 <dydwn0928@gmail.com>
    
    Merge-request: TELESCOPE-MR-14
    Merged-by: 윤창 최 <ycc4477@gmail.com>

--- components/main/Dashboard.tsx
+++ components/main/Dashboard.tsx
@@ -11,7 +11,8 @@ function Dashboard({ rankingsResponse }: IDashboard) {
   return (
     <div className={`flex pt-[26px] px-[55px] gap-[14px]`}>
       <div className={`w-[869px]`}>
-        <SummaryStat></SummaryStat>
+        <SummaryStat total = {1} create_issue = {2} resolve_issue = {3}
+        create_code_review = {4} merge_mr = {5} ></SummaryStat>
         <Ranking rankings={rankingsResponse?.rankings}></Ranking>
       </div>
       <div>
--- components/main/SummaryStat.tsx
+++ components/main/SummaryStat.tsx
@@ -1,9 +1,35 @@
-function SummaryStat() {
+function DivSummary({ number, text, color }: any) {
   return (
-    <div
-      className={`w-full h-[111px] shadow-[0_5px_20px_1px_rgba(0,0,0,0.1)] rounded mb-[12px]`}
-    >
-      상단 요약 통계 컴포넌트
+    <div className="flex flex-col	items-center items-center gap-1">
+      <span className="font-bold text-[45px]"
+      style={{color : color}}>
+        {number}
+      </span>
+      <span className="font-bold text-[9px] text-[#9A9EA6]">
+        {text}
+      </span>
+    </div>
+  )
+}
+
+function SummaryStat({ total, create_issue, resolve_issue, create_code_review, merge_mr }: any) {
+  return (
+    <div>
+      <span className="w-[163px] h-[23px]">
+        Team Productivity
+      </span>
+      
+      <div
+        className={`w-full h-[111px] flex justify-center shadow-[0_5px_20px_1px_rgba(0,0,0,0.1)] rounded mb-[12px]`}
+      >
+        <div className="w-[733px] flex justify-center items-center flex-row items-start gap-[94px]">
+          {DivSummary({ number: total, text: 'total', color:'#8000FF' })}
+          {DivSummary({ number: create_issue, text: 'create_issue', color:'#00FF38' })}
+          {DivSummary({ number: resolve_issue, text: 'resolve_issue', color:'#3FF7C0' })}
+          {DivSummary({ number: create_code_review, text: 'create_code_review', color:'#E9488B' })}
+          {DivSummary({ number: merge_mr, text: 'merge_mr', color:'#387AF1' })}
+        </div>
+      </div>
     </div>
   )
 }
