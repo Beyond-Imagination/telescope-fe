ddca446215686695862df2d76adaedbe074441f0
laggu91 <laggu91@gmail.com>
2023-06-22 11:07:00 +0000

    style: lint
    Merge-request: TELESCOPE-MR-210
    Merged-by: 구원 라 <laggu91@gmail.com>

--- components/personal/index.tsx
+++ components/personal/index.tsx
@@ -4,8 +4,7 @@ import Information from '../common/Information'
 import DateSelector from '../main/DateSelector'
 import { ScoreBoard, TotalScoreBoard } from '../common/ScoreBoard'
 
-
-function Personal({organizationName, userData, scoreData, profileMap, timeType, setTimeType }: any) {
+function Personal({ organizationName, userData, scoreData, profileMap, timeType, setTimeType }: any) {
     useEffect(() => {}, [timeType])
     const colors: any = {
         createIssue: '#F2994A',
@@ -24,20 +23,18 @@ function Personal({organizationName, userData, scoreData, profileMap, timeType, 
                 <div className={`flex justify-between`}>
                     <div className={`flex`}>
                         <span>
-                            {
-                                userData?.profilePicture ? (
-                                    <div>
-                                        <img
-                                            className={`rounded-[20px]`}
-                                            src={profileMap.get(userData.profilePicture)}
-                                            style={{ height: 40, width: 40 }}
-                                            alt="picture"
-                                        />
-                                    </div>
-                                ) : (
-                                    <Jdenticon size="40" value={userData?.name ? `${userData.name.firstName} ${userData.name.lastName}` : 'Nickname'} />
-                                )
-                            }
+                            {userData?.profilePicture ? (
+                                <div>
+                                    <img
+                                        className={`rounded-[20px]`}
+                                        src={profileMap.get(userData.profilePicture)}
+                                        style={{ height: 40, width: 40 }}
+                                        alt="picture"
+                                    />
+                                </div>
+                            ) : (
+                                <Jdenticon size="40" value={userData?.name ? `${userData.name.firstName} ${userData.name.lastName}` : 'Nickname'} />
+                            )}
                         </span>
                         <span className={`font-normal text-[32px] color-[#23222c] mx-2`} style={{ fontWeight: 600 }}>
                             {userData?.name ? `${userData.name.firstName} ${userData.name.lastName}` : 'Nickname'}
@@ -49,11 +46,7 @@ function Personal({organizationName, userData, scoreData, profileMap, timeType, 
                     <div>
                         <div className={`grid grid-cols-4 gap-2`}>
                             <TotalScoreBoard score={scoreData ? scoreData.score.total : 0} />
-                            <ScoreBoard
-                                color={colors.createIssue}
-                                score={scoreData ? scoreData.score.createIssue : 0}
-                                title={'Create<br/>Issues'}
-                            />
+                            <ScoreBoard color={colors.createIssue} score={scoreData ? scoreData.score.createIssue : 0} title={'Create<br/>Issues'} />
                             <ScoreBoard
                                 color={colors.resolveIssue}
                                 score={scoreData ? scoreData.score.resolveIssue : 0}
@@ -65,19 +58,13 @@ function Personal({organizationName, userData, scoreData, profileMap, timeType, 
                                 title={'Create<br/>Code Review'}
                             />
                             <ScoreBoard color={colors.mergeMr} score={scoreData ? scoreData.score.mergeMr : 0} title={'Merge<br/>MR'} />
-                            <ScoreBoard
-                                color={colors.receiveStar}
-                                score={scoreData ? scoreData.score.receiveStar : 0}
-                                title={'Receive<br/>Star'}
-                            />
+                            <ScoreBoard color={colors.receiveStar} score={scoreData ? scoreData.score.receiveStar : 0} title={'Receive<br/>Star'} />
                         </div>
                     </div>
                 </div>
             </div>
 
-            <div className={`w-full`}>
-                star 사용법 추가
-            </div>
+            <div className={`w-full`}>star 사용법 추가</div>
         </div>
     )
 }
