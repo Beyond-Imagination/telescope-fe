79f48c7fd11361e24c59e772851510fff33a2857
laggu91 <laggu91@gmail.com>
2023-08-06 11:09:27 +0000

    fix: pie chart inner text position
    Merge-request: TELESCOPE-MR-243
    Merged-by: 구원 라 <laggu91@gmail.com>

--- components/organization/Organization.tsx
+++ components/organization/Organization.tsx
@@ -151,7 +151,7 @@ export default function Organization({ organizationName, summaryResponse, rankin
                         total={summaryResponse?.score?.total}
                         chartColors={teamChartColors}
                         legend="right"
-                        innerTextLeftPosition={-20}
+                        innerTextLeftPosition={-22}
                     ></PieChart>
 
                     <PieChart
@@ -160,7 +160,7 @@ export default function Organization({ organizationName, summaryResponse, rankin
                         total={summaryResponse?.score?.total}
                         chartColors={rankingsResponse?.rankings?.length > 0 ? null : ['#eeeeee']}
                         legend="right"
-                        innerTextLeftPosition={-20}
+                        innerTextLeftPosition={-22}
                     ></PieChart>
                 </div>
 
